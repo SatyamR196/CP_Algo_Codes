b4e1d6b6457426092cb429898e00692b4fff830a
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2024-10-13 18:37:59 +0530

    Update NGE_without_stack.cpp

--- Next_Greater_Element/NGE_without_stack.cpp
+++ Next_Greater_Element/NGE_without_stack.cpp
@@ -1,8 +1,10 @@
 vector<int> nextLargerElement(vector<int> arr, int n){
         vector<int>nge(n,-1);
         for(int i=n-1;i>=0;i--){
-            nge[i]=i+1;
-            while(nge[i]!=n && arr[i]>=arr[nge[i]]){
+            nge[i]=i+1;//doing this we assume that next element is our NGE
+            while(nge[i]!=n && arr[i]>=arr[nge[i]]){// but if this conditon is true then our assumption is wrong
+                //hence the current element says my NGE = my next element NGE ;
+                //This continues till current element got it's actual NGE and while loop exits.
                 nge[i]=nge[nge[i]];
             }
         }
