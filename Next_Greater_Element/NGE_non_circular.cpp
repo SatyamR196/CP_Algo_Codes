03be01c2e172de1048979a01f0ef82a89607d487
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2024-10-13 18:32:56 +0530

    Create NGE_non_circular.cpp

---
+++ Next_Greater_Element/NGE_non_circular.cpp
@@ -0,0 +1,14 @@
+vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>nge(n,-1);
+        stack<int>st;
+        for(int i=0;i<2*n;i++){
+            while(!st.empty() && nums[st.top()]<nums[idx]){
+                nge[st.top()]=nums[idx];
+                // nge[st.top()]=idx; //If index of NGE is needed;
+                st.pop();
+            }
+            st.push(idx);
+        }    
+        return nge;
+}
