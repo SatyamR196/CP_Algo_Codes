36462b41a87557f8ceba4197fcf60581f8989b8b
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2024-12-06 07:29:40 +0530

    Update SW_forLoop.cpp

--- Sliding_Window/SW_forLoop.cpp
+++ Sliding_Window/SW_forLoop.cpp
@@ -1,13 +1,29 @@
 for(int i=0;i<n;i++){
-      if(arr[i]==0)cnt0++;
-      else cnt1++;
       
-      if(i-wSize>=0){
-         if(arr[i-wSize]==0) cnt0--;
-         else cnt1--;
+//Action to be performed always like insertion in window. 
+      
+      if(i-wSize+1>=0){
+      
+      //Main actions to be performed in every window once window is of desired size like printing sum of window elements.
+      
+      //Remove the effect of first window element for next slide e.g. deletion of element from bag.
+      
       }
-      if(i-wSize>=-1){
-        //inside here goes any action you want to
-        //perform for every window
-      }
+}
+
+// Example 1 : where bag is a custom data structure supporting insert,remove,getMax fxn;
+for(int i=0;i<n;i++){ 
+      bag.insert(arr[i]);
+      if(i-wSize+1>=0){
+            cout<<bag.getMax()<<" "; 
+            bag.remove(arr[i-wSize+1]);  
+      } 
+}
+// Example 2 : Counting 0s and 1s in every window;
+for(int i=0;i<n;i++){
+  arr[i]==0 ? count0++ : count1++ ;
+  if(i-wSize+1>=0){
+      cout<<"count 0 = "<<count0<<" , "<<"count 1 = "<<count1<<endl; 
+      arr[i]==0 ? count0-- : count1-- ;
+  } 
 }
