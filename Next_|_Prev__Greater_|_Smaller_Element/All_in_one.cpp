925073b22f241e7a966b872d2be89b6f21271e83
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2024-12-12 09:01:49 +0530

    Update All_in_one.cpp

--- Next_|_Prev__Greater_|_Smaller_Element/All_in_one.cpp
+++ Next_|_Prev__Greater_|_Smaller_Element/All_in_one.cpp
@@ -1,6 +1,6 @@
 // This code can be used to find NGE,PGE,NSE and PSE, just choose appropriate pair of for_loop and while_loop and uncomment them.
   stack<int>st;
-  vector<intv(n,-1);
+  vector<int>v(n,-1);
 
 // for(int i=0 ; i<n ;i++){ Next...
 // for(int i=n-1 ;i>=0 ;i--){ Prev...
