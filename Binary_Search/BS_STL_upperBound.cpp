b22465365a49cfc27d55bbd63cc0855ce2854f34
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2024-12-12 22:05:59 +0530

    Update BS_STL_upperBound.cpp

--- Binary_Search/BS_STL_upperBound.cpp
+++ Binary_Search/BS_STL_upperBound.cpp
@@ -1,6 +1,17 @@
 int search(vector<int>& nums,int target) {
         int index= upper_bound(nums.begin(),nums.end(),target)-nums.begin(); // if array is sorted in non decreasing order
-        int index= upper_bound(nums.begin(),nums.end(),target,greater<int>())-nums.begin(); // if array is sorted in non increasing order
+        // int index= upper_bound(nums.begin(),nums.end(),target,greater<int>())-nums.begin(); // if array is sorted in non increasing order
+        if(index==0) return -1;
+        if(nums[index-1]==target){
+            return index-1;
+        }else{
+            return -1;
+        }
+}
+// Range of search [low,high) version
+int search(vector<int>& nums,int low,int high,int target) {
+        int index= upper_bound(nums.begin()+low,nums.begin()+high,target)-nums.begin(); // if array is sorted in non decreasing order
+        // int index= upper_bound(nums.begin()+low,nums.begin()+high,target,greater<int>())-nums.begin(); // if array is sorted in non increasing order
         if(index==0) return -1;
         if(nums[index-1]==target){
             return index-1;
