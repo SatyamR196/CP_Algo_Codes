6e38c853b5baee359aa8bfe5a5acac2c13778478
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2024-12-15 17:07:24 +0530

    Update BS_realDomain.cpp

--- Binary_Search/BS_realDomain.cpp
+++ Binary_Search/BS_realDomain.cpp
@@ -2,7 +2,7 @@
 // e = (precision or max error allowed) / 100 just to be safe we divide by 100.
 // For example, if given precision e<= 1e-6 then we should take e=1e-8 ;
 long double low = 0,high=n ;
-while(abs(high-low)< e){ 
+while(abs(high-low) > e){ 
   long double mid=low+(high-low)/2;
   if(check(mid)){
     high=mid;
