0588683fa3a5af6f13d77110555c0a5b9aa33ab2
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2025-02-27 21:36:25 +0530

    Create Topological_sort_lexicographical_BFS.cpp

---
+++ Topological_sort/Topological_sort_lexicographical_BFS.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+vector<vector<int>>graph;
+vector<int>indeg;
+vector<int>topo;
+
+void Kahn_topo(){
+  // priority_queue used for topo in lexicographical order
+  priority_queue<int,vector<int>,greater<int>>q ; // min_heap
+  for(int i=1;i<=n;i++){
+    if(indeg[i]==0) q.push(i);
+  }
+  
+  while(!q.empty()){
+    int cNode = q.top() ; 
+    q.pop() ;
+    for(auto ele : graph[cNode]){
+      indeg[ele]--;
+      if(indeg[ele]==0) q.push(ele) ;
+    }
+    topo.push_back(cNode) ;
+  }
+}
+
+int main() 
+{
+  cin>>n>>m;
+  graph.resize(n+1);
+  indeg.resize(n+1);
+  
+  for(int i=0;i<m;i++){
+    int a,b;
+    cin>>a>>b;
+    graph[a].push_back(b);
+    indeg[b]++;
+  }
+  
+  Kahn_topo();
+  
+  for(auto ele : topo){
+    cout<<ele<<" ";
+  }
+}
