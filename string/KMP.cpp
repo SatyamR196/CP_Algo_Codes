43fe9efd8d8a68be0421f026e7b06b4ea58f1c6c
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2025-06-05 13:38:09 +0530

    Update KMP to LPS.cpp

--- string/KMP.cpp
+++ string/KMP.cpp
@@ -1,12 +1,13 @@
-vector<int> get_kmp(string str){
+// Longest Prefix Suffix(LPS) : lps[i] = length of longest suffix which is equal to prefix till index i of string for which LPS is calculated.
+vector<int> get_lps(string str){
   int n = str.length() ;
-  vector<int>kmp(n+1) ;
+  vector<int>lps(n+1) ;
   
-  int i = 0 , j = -1 ; kmp[0] = -1 ;
+  int i = 0 , j = -1 ; lps[0] = -1 ;
   while(i<n){
-    while(j!= -1 && str[i]!=str[j]) j = kmp[j] ;
+    while(j!= -1 && str[i]!=str[j]) j = lps[j] ;
     j++ ; i++ ;
-    kmp[i] = j ;
+    lps[i] = j ;
   }
-  return kmp ; 
+  return lps ; 
 }
