61bb79b112dd328286225c7b80959093677d9d9a
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2025-06-07 11:02:48 +0530

    Create seive_of_eratosthenes.cpp

---
+++ Number_Theory/seive_of_eratosthenes.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>seive(int n){
+  vector<int>num(n+1,1) ;
+  num[0] = 0 ;
+  num[1] = 0 ;
+  for(int i=2 ;i<=n ;i++){
+    if(num[i]){
+      for(int j = 2*i ; j<=n ; j+=i) num[j] = 0 ;
+    }
+  }
+  
+  return num ;
+}
+
+vector<int>seive_opt(int n){
+  vector<int>num(n+1,1) ;
+  num[0] = 0 ;
+  num[1] = 0 ;
+  for(int i=2 ;i<=n ;i++){
+    if(num[i]){
+      for(long long j = 1LL*i*i ; j<=n ; j+=i) num[j] = 0 ;
+    }
+  }
+  
+  return num ;
+}
+
+int main() 
+{
+  auto v = seive_opt(50) ;
+  for(auto ele : v){
+    cout<<ele<<" ";
+  }
+  return 0;
+}
