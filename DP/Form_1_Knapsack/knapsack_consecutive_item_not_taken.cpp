cac62906e8172f89bfd5a9a214965338fba61b05
Satyam Rahangdale <157214562+SatyamR196@users.noreply.github.com>
2025-06-12 17:22:32 +0530

    Create knapsack_consecutive_item_not_taken.cpp

---
+++ DP/Form_1_Knapsack/knapsack_consecutive_item_not_taken.cpp
@@ -0,0 +1,81 @@
+//_ DP Form 1 : Knapsack : Choose a subset which satisfies the given conditions
+//! Key Idea : Iterate over list of items, then either Choose to pick or reject for each items
+// Link : https://onecompiler.com/cpp/43mhxfmzy
+//? Knapsack with restriction that if item i is taken then item i+1 can't be taken . 
+//! Idea : 
+//? 1) Method 1 : modify transition, if you take i, then clearly you can go to i+1,
+//? so call dp(i+2) when you pick an item i.
+//~ 1) Method 2 : modify state, add a bool parameter last_taken,
+//~ when you taken item,call dp(i+1, last_taken =true), when you don't take,
+//~ dp(i+1,last_taken=false) add check that take item only if is last_taken=false ;
+//~ note is will add another dimentsion to cache array.
+
+#include<bits/stdc++.h> 
+using namespace std;
+
+int INF = 1e9 ;
+int wi[21] ;
+int vi[21] ;
+int N,W ;
+int cache[21][101] ; //~ <level,weight>
+int dp(int level,int w_left){
+    //base case
+    if(level == N+1) return 0 ;
+  
+    // cache check
+    if(cache[level][w_left] != -1) return cache[level][w_left] ;
+
+    //transiton :
+    int ans = -INF ;
+    //choice 1 : don't take
+    ans = max(ans,dp(level+1,w_left)) ;
+    //choice 2 : take it in
+    if(wi[level] <= w_left){
+        ans = max(ans, vi[level] + dp(level+2, w_left - wi[level])) ;
+    }
+
+    //save and return ans ;
+    return cache[level][w_left] = ans ;
+}
+
+//! This fxn return the item we need to pick for max Value within weight limits.
+vector<int> print_items(){
+  cout<<"item_no : ";
+  int w_left = W ;
+  vector<int>itemTaken ;
+  
+  for(int i=1 ; i<=N ; i++){
+    int dontakeVal = dp(i+1,w_left) ;
+    int takeVal = (w_left>=wi[i]) ? vi[i]+dp(i+1,w_left-wi[i]) : 0 ;
+    
+    if(takeVal >= dontakeVal){
+      itemTaken.push_back(i) ;
+      w_left -= wi[i] ;
+      cout<<i<<", "; 
+    }
+    
+  }
+  cout<<endl;
+  return itemTaken ;
+}
+
+void solve(){    
+    cin>>N>>W ;
+    for(int i=1;i<=N;i++){
+        cin>>wi[i]>>vi[i] ;
+    }
+    memset(cache,-1,sizeof(cache)) ;
+    // debug(cache) ;
+    cout<<dp(1,W)<<endl ;
+    // print_items() ;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
