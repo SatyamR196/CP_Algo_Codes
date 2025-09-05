21779f2a765372626a45c2c9f71101ffcd1d5356
Satyam Rahangdale <satyamrahangdale196@gmail.com>
2025-09-05 08:20:46 +0530

    Update BS_STL_upperBound.cpp

--- Binary_Search/BS_STL_upperBound.cpp
+++ Binary_Search/BS_STL_upperBound.cpp
@@ -1,3 +1,5 @@
+// oneCompiler Link : https://onecompiler.com/cpp/43vsezuwk
+
 int search(vector<int>& nums,int target) {
         int index= upper_bound(nums.begin(),nums.end(),target)-nums.begin(); // if array is sorted in non decreasing order
         // int index= upper_bound(nums.begin(),nums.end(),target,greater<int>())-nums.begin(); // if array is sorted in non increasing order
